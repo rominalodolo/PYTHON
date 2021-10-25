88c13eae697436ae423a3185d0df05ec28969d72
rominalodolo <lodoloromina@gmail.com>
2021-10-25 15:43:54 +0200

    changes lineChart

--- PythonMatplotlib/lineChart.py
+++ PythonMatplotlib/lineChart.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
 x = [1,2,3,4]
-y = [2,4,6,8,14,16,4]
+y = [2,4,6,8]
+# x and y values must correlate with one another ie same number of values for each 
 
-plt.plot(x, y, 'y^')
+plt.plot(x, y, 'r-')
 # To creat different types/ colors of lines b-. or r--  or y:  y^  ect.
 # you can also specify line width   linewidth=4.0
 plt.axis([0,5,0,10])
