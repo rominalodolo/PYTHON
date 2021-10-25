c8a7988cda8271485378a8657e2032549d89baa9
rominalodolo <lodoloromina@gmail.com>
2021-10-25 20:31:54 +0200

    grid update

--- PythonMatplotlib/MorePlots/grid.py
+++ PythonMatplotlib/MorePlots/grid.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 x1 = [1,2,3,4,5]
 y1 = [1,2,4,8,16]
 
-plt.plot(x1, y1, 'ro--', label='students')
+plt.plot(x1, y1, 'mo--', label='Makeup Students')
 plt.legend(loc='best')
-plt.title('Your title')
-plt.xlabel('Horizontal title')
-plt.ylabel('Vertical title')
-plt.grid(which='major',linestyle='-', color='black')
+plt.title('All the Students from Lovelies Beauty Academy')
+plt.xlabel('Classes')
+plt.ylabel('Amount of Students')
+plt.grid(which='major',linestyle='-', color='#FD34BB')
 plt.minorticks_on()
-plt.grid(which='minor',linestyle='--', color='gray', alpha=0.2)
+plt.grid(which='minor',linestyle='--', color='#FFC8ED', alpha=0.5)
 plt.show()
