2d700414c269103f3f88815cc10ed153138c3dad
rominalodolo <lodoloromina@gmail.com>
2021-10-25 20:14:44 +0200

    pink bar chart

--- PythonMatplotlib/MorePlots/bar.py
+++ PythonMatplotlib/MorePlots/bar.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 x1 = [1,2,3,4,5]
 y1 = [1,2,4,8,16]
 
-colors = [ 'green', 'red', 'blue', 'orange', 'lightblue']
+colors = [ '#FFC8ED', '#FFAAE3', '#FF90DA', '#FC65CA', '#FD34BB']
 
-plt.bar(x1,y1,edgecolor='black',color=colors, linewidth=2)
-plt.title('Your title')
+plt.bar(x1,y1,edgecolor='aqua',color=colors, linewidth=2)
+plt.title('Bar Chart')
 plt.xlabel('Horizontal title')
 plt.ylabel('Vertical title')
 plt.show()
+
