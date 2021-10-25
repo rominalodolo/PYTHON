03fbb40e3007bd83dfccc8955d5d763581baee86
rominalodolo <lodoloromina@gmail.com>
2021-10-25 20:19:16 +0200

    Pink Pie Chart

--- PythonMatplotlib/MorePlots/pie.py
+++ PythonMatplotlib/MorePlots/pie.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
 values = [16,18,4,8]
-pielabels = ['Python', 'Ruby', 'Java', 'Perl']
-piecolors = ['red','orange','yellow','maroon']
+pielabels = ['Malibu', 'Barbie', 'Pink', 'Girly']
+piecolors = ['#FFC8ED','#FFAAE3','#FF90DA','#FC65CA']
 pieexplode = [0,0.1,0,0]
 
 plt.pie(values, labels=pielabels,explode=pieexplode,colors=piecolors,startangle=0,shadow=True)
-plt.title('Pie Chart')
+plt.title('Pink Pie Chart')
 plt.xlabel('Horizontal title')
 plt.ylabel('Vertical title')
 plt.legend(loc='best')
