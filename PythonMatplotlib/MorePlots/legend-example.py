59decab70a64db0b883e77b220dfcc84a14ad820
rominalodolo <lodoloromina@gmail.com>
2021-10-25 20:47:22 +0200

    ledgend update

--- PythonMatplotlib/MorePlots/legend-example.py
+++ PythonMatplotlib/MorePlots/legend-example.py
@@ -4,14 +4,14 @@ x1 = [1,2,3,4,5]
 y1 = [1,2,4,8,16]
 y2 = [1,1,2,3,5]
 
-plt.plot(x1, y1, 'ro-', label='students')
-plt.plot(x1, y2, 'b^-', label='teachers')
+plt.plot(x1, y1, 'mo-', label='students')
+plt.plot(x1, y2, 'y^-', label='teachers')
 
 plt.subplots_adjust(left=0.3, bottom=0.3)
-plt.legend(bbox_to_anchor=(-0.15, 0.17), loc='upper right')
+plt.legend(bbox_to_anchor=(-0.15, 0.17), loc='best')
 
-plt.title('Your title')
-plt.xlabel('Horizontal title')
-plt.ylabel('Vertical title')
+plt.title('School Attendance during Covid')
+plt.xlabel('Days')
+plt.ylabel('Total number pressent')
 plt.grid(True)
 plt.show()
