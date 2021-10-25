92016bfbd6ec0466e77860badc041803e2848174
rominalodolo <lodoloromina@gmail.com>
2021-10-25 17:43:32 +0200

    fill update

--- PythonMatplotlib/Introduction/fill.py
+++ PythonMatplotlib/Introduction/fill.py
@@ -5,6 +5,6 @@ X = np.arange(0, 10)
 Y = 0.05*X*X
 
 plt.ylim(0, 5)
-plt.plot(X, Y, color='blue')
-plt.fill_between(X, Y, color='red', alpha='0.5')
+plt.plot(X, Y)
+plt.fill_between(X, Y, color='blue', alpha='0.5')
 plt.show()
