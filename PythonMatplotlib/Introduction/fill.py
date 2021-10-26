cb51cc179aa0e77c6c864544ee843188bba93ddc
rominalodolo <lodoloromina@gmail.com>
2021-10-26 16:14:17 +0200

    error fixed on fill files

--- PythonMatplotlib/Introduction/fill.py
+++ PythonMatplotlib/Introduction/fill.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-X = np.arange(0, 10)
+X = np.arange(0,10)
 Y = 0.05*X*X
 
 plt.ylim(0, 5)
 plt.plot(X, Y)
-plt.fill_between(X, Y, color='blue', alpha='0.5')
+plt.fill_between(X, Y, color='#FF90DA', alpha=1)
+
 plt.show()
