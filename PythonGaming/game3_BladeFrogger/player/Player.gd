d61168ec0f1e4f4a9dc630f2ca1e73477d9380f2
rominalodolo <lodoloromina@gmail.com>
2021-10-29 18:59:22 +0200

    script final
    adding the final script for player

--- PythonGaming/game3_BladeFrogger/player/Player.gd
+++ PythonGaming/game3_BladeFrogger/player/Player.gd
@@ -1,16 +1,30 @@
 extends KinematicBody2D
 
+const speed = 200 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	move_and_slide(velocity.normalized() * speed)
+	player_animation(velocity)
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func player_animation(velocity):
+	if velocity.y > 0:
+		$"AnimatedSprite".play('walk_down')
+	elif velocity.y < 0:
+		$"AnimatedSprite".play('walk_up')
+	
+	elif velocity.x != 0:
+		$"AnimatedSprite".play('walk_side')
+		$"AnimatedSprite".flip_h = velocity.x > 0
+	else:
+		$"AnimatedSprite".stop()
+		$"AnimatedSprite".frame = 0
