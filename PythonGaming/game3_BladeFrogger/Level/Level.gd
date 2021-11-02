5861b24b6de4141c036198825cbe85212b2652b6
rominalodolo <lodoloromina@gmail.com>
2021-11-02 14:09:09 +0200

    Adding player position
    Adding player position to the level

--- PythonGaming/game3_BladeFrogger/Level/Level.gd
+++ PythonGaming/game3_BladeFrogger/Level/Level.gd
@@ -2,24 +2,18 @@ extends Node
 
 const CAR = preload("res://car/car.tscn")
 
+func _ready():
+	$Player.position = $PlayerStartPos.position
 
-func car_spawn():	
+func car_spawn(start_pos):	
 	var car = CAR.instance()
-	car.position = $"StartPositions/CarStart1".position
+	var car_start_node_name = "StartPositions/CarStart" + str(start_pos)
+	car.position = get_node(car_start_node_name).position
 	add_child(car)	
 
 
-func _on_Timer1_timeout():
-	car_spawn()
-
-func _on_Timer2_timeout():
-	pass # Replace with function body.
-
-func _on_Timer3_timeout():
-	pass # Replace with function body.
-
-func _on_Timer4_timeout():
-	pass # Replace with function body.
-
-func _on_Timer5_timeout():
-	pass # Replace with function body.
+func _on_Timer1_timeout(): car_spawn(1)
+func _on_Timer2_timeout(): car_spawn(2)
+func _on_Timer3_timeout(): car_spawn(3)
+func _on_Timer4_timeout(): car_spawn(4)
+func _on_Timer5_timeout(): car_spawn(5)
